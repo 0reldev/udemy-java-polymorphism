a3f7e63f56cfd2778ad67453951721cfc13bb663
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-06-08 14:32:45 +0200

    Lesson part 1

---
+++ src/main/java/Movie.java
@@ -0,0 +1,52 @@
+public class Movie {
+
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void watchMovie() {
+        String instanceType = this.getClass().getSimpleName();
+        System.out.println(title + " is a " + instanceType + " film");
+    }
+}
+
+class Adventure extends Movie {
+
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3), "Pleasant scene", "Scary music", "Something bad happens");
+    }
+}
+
+class Comedy extends Movie {
+
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3), "Something fun happens", "Something even funnier happens", "Happy ending");
+    }
+}
+
+class ScienceFiction extends Movie {
+
+    public ScienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3), "Bad aliens do bad stuff", "Space guys chase aliens", "Planets blows up");
+    }
+}
